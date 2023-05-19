423e60b26cda17cd7b2ba190dce74035803839a2
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-19 17:22:05 -0600

    feat(Home page): add list of options

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
   import { useLogging } from './../composables/use-logging'
+  import Card from './../components/Card.vue'
+  import Button from './../components/Button.vue'
   useLogging()
 </script>
 
 <template>
-  <Card class="m-4 h-full">
+  <Card class="m-4 h-full flex flex-col gap-4">
+    <Button color="primary" class="w-full">Crear lugar</Button>
+    <RouterLink to="/parking-places">
+      <Button color="primary" class="w-full">
+          Listado de lugares
+      </Button>
+    </RouterLink>
   </Card>
 </template>
