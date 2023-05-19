3042f3fed5b4d2b42e755c249bd33c75c16d137e
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-19 17:26:02 -0600

    feat(Router): register `/parking-places` route and page

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import Home from './pages/Home.vue'
 import LogIn from './pages/LogIn.vue'
+import ParkingPlaces from './pages/ParkingPlaces.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
-    { path: '/login', component: LogIn, name: 'LogIn' }
+    { path: '/login', component: LogIn, name: 'LogIn' },
+    { path: '/parking-places', component: ParkingPlaces, name: 'ParkingPlaces' }
   ]
 })
 
