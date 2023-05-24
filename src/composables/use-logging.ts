6c1ff0cb3783ac1a71d7f61df52ce6dfdef0fa65
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-24 16:58:30 -0600

    feat(logging composable): verify token in API

--- src/composables/use-logging.ts
+++ src/composables/use-logging.ts
@@ -1,11 +1,18 @@
 import { onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+import axios from '../common/axios'
 
 export function useLogging () {
-  onMounted(() => {
+  onMounted(async () => {
+    const token = localStorage.getItem('token')
     const route = useRoute()
     const router = useRouter()
-    if (route.name !== 'logging' && localStorage.getItem('credential') === null) {
+    const res = await axios.get<'ok'>('/auth', {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+    if (route.name !== 'logging' && token === null && res.status !== 200) {
       router.push({ name: 'LogIn', query: { redirect: route.path } })
     }
   })
