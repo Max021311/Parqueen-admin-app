fee1ea22e61b3131c6cd334f80117a1842b03890
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2023-05-24 22:04:22 -0600

    feat(Toggle component): use `Switch` from `headlessui`

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -1,39 +1,43 @@
 <template>
-  <button
+  <Switch
+    :modelValue="modelValue"
+    @update:model-value="handler"
     class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
     :class="buttonClass"
-    @click="handleClick"
   >
-    <span class="sr-only">Use setting</span>
-    <span
-      aria-hidden="true"
+    <input
+      type="checkbox"
+      :value="modelValue"
+      class="sr-only"
+    >
+    <span aria-hidden="true"
       class="pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
       :class="circleClass"
     />
-  </button>
+  </Switch>
 </template>
 
 <script setup lang="ts">
   import { computed } from 'vue'
-  import Button from './Button.vue'
+  import { Switch } from '@headlessui/vue'
 
   interface Props {
-    value: boolean
+    modelValue: boolean
   }
 
   const props = withDefaults(
     defineProps<Props>(),
-    { value: false }
+    { modelValue: false }
   )
 
   const emit = defineEmits<{
-    (e: 'click', value: boolean): void
+    (e: 'update:modelValue', value: boolean): void
   }>()
 
-  const buttonClass = computed(() => props.value ? 'bg-emerald-500' : 'bg-gray-100')
-  const circleClass = computed(() => props.value ? 'translate-x-5' : 'translate-x-0')
+  const buttonClass = computed(() => props.modelValue ? 'bg-emerald-500' : 'bg-gray-100')
+  const circleClass = computed(() => props.modelValue ? 'translate-x-5' : 'translate-x-0')
 
-  function handleClick () {
-    emit('click', !props.value)
+  function handler (newValue: boolean) {
+    emit('update:modelValue', newValue)
   }
 </script>
